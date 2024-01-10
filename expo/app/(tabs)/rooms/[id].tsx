2d6d1a00f6a1c1d1049cf0ef88fe91c056730dbe
ngomesodent <101274479+ngomesodent@users.noreply.github.com>
2024-01-10 19:50:25 +0100

    fix: change deep link that weren't working

--- expo/app/(tabs)/rooms/[id].tsx
+++ expo/app/(tabs)/rooms/[id].tsx
@@ -1,20 +1,31 @@
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, Text } from "react-native";
+import { useEffect, useState } from "react";
 import Button from "../../../components/Button";
 import * as Linking from "expo-linking";
 import * as WebBrowser from "expo-web-browser";
 import * as Clipboard from "expo-clipboard";
 
 export default function RoomPage() {
-  const onOpenLink = async () => {
-    //WebBrowser.openBrowserAsync("https://docs.expo.io");
+  //Listening to any incoming deep link
+  useEffect(() => {
+    const subscription = Linking.addEventListener("url", handleDeepLink);
+    return (() => {
+      subscription.remove();
+    })
+  });
+  
+  //Handler
+  const handleDeepLink = (event: any) => {};
+
+  const onShareLink = async () => {
     const roomCode = "A1B2C3"
-    const deepLink = Linking.createURL(`join/${roomCode}`, {scheme: "datsmysong"});
+    const deepLink = Linking.createURL(`rooms/${roomCode}`);
     await Clipboard.setStringAsync(deepLink);
   };
 
   return (
     <View style={styles.shareContainer}>
-      <Button label="Partager" theme="filled" onPress={onOpenLink}></Button>
+      <Button label="Partager" theme="filled" onPress={onShareLink}></Button>
     </View>
   );
 }
