b5a1feea0e5c21cbe4e8730885b5b16977ddbd10
ngomesodent <101274479+ngomesodent@users.noreply.github.com>
2024-01-11 13:56:14 +0100

    feat: beginning of opening a opening a web page when clicking the invitation link

--- expo/app/(tabs)/rooms/[id].tsx
+++ expo/app/(tabs)/rooms/[id].tsx
@@ -6,6 +6,8 @@ import * as WebBrowser from "expo-web-browser";
 import * as Clipboard from "expo-clipboard";
 
 export default function RoomPage() {
+  const currentPageLink = Linking.useURL();
+
   //Listening to any incoming deep link
   useEffect(() => {
     const subscription = Linking.addEventListener("url", handleDeepLink);
@@ -18,9 +20,18 @@ export default function RoomPage() {
   const handleDeepLink = (event: any) => {};
 
   const onShareLink = async () => {
-    const roomCode = "A1B2C3"
-    const deepLink = Linking.createURL(`rooms/${roomCode}`);
-    await Clipboard.setStringAsync(deepLink);
+    // For when the user click on "Open in the app" in the web browser
+    // const roomCode = "A1B2C3"
+    // const deepLink = Linking.createURL(`rooms/${roomCode}`);
+    let invitationLink: string;
+    if (process.env.NODE_ENV === "production") {
+      currentPageLink ?
+      invitationLink = currentPageLink.split("//")[1] :
+      invitationLink = "";
+    } else {
+
+    }
+    //await Clipboard.setStringAsync();
   };
 
   return (
