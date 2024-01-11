24681c1f18e2468c124b7548dd99dc356d32af8d
GaspardBBY <96123850+GaspardBBY@users.noreply.github.com>
2024-01-11 11:03:36 +0100

    fix: misstake with expo Screen

--- expo/app/(auth)/index.tsx
+++ expo/app/(auth)/index.tsx
@@ -6,29 +6,27 @@ import { Divider } from "react-native-elements";
 
 export default function OnBoarding() {
   return (
-    <Screen>
-      <View style={styles.container}>
-        <View style={styles.titleContainer}>
-          <Text style={styles.title}>datsmysong</Text>
-        </View>
-        <View style={styles.container}>
-          <ConnectWithSpotify />
-          <Pressable onPress={() => router.push("/ask-name")}>
-            <Text>Rejoindre avec Google</Text>
-          </Pressable>
-          <View style={styles.containerWithDivider}>
-            <Divider style={{ width: "80%", margin: 20 }} />
-            <Text>ou</Text>
-            <Divider style={{ width: "80%", margin: 20 }} />
-          </View>
-          <Link href={"/(auth)/login"} asChild>
-            <Pressable>
-              <Text>Rejoindre avec une adresse email</Text>
-            </Pressable>
-          </Link>
-        </View>
+    <View style={styles.container}>
+      <View style={styles.titleContainer}>
+        <Text style={styles.title}>Datsmysong</Text>
       </View>
-    </Screen>
+      <View style={styles.container}>
+        <ConnectWithSpotify />
+        <Pressable onPress={() => router.push("/ask-name")}>
+          <Text>Rejoindre avec Google </Text>
+        </Pressable>
+        <View style={styles.containerWithDivider}>
+          <Divider style={{ width: "80%", margin: 20 }} />
+          <Text>ou</Text>
+          <Divider style={{ width: "80%", margin: 20 }} />
+        </View>
+        <Link href={"/(auth)/login"} asChild>
+          <Pressable>
+            <Text>Rejoindre avec une adresse email</Text>
+          </Pressable>
+        </Link>
+      </View>
+    </View>
   );
 }
 
