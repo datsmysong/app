798cd832eb63cc71dc71af76d613d5b46fb96b3c
MAXOUXAX <24844231+MAXOUXAX@users.noreply.github.com>
2024-01-11 11:55:13 +0100

    feat: improved RoomView

--- expo/app/(tabs)/rooms/[id].tsx
+++ expo/app/(tabs)/rooms/[id].tsx
@@ -1,18 +1,29 @@
 import { useLocalSearchParams } from "expo-router";
 import ActiveRoomView from "../../../components/ActiveRoomView";
-import { Text } from "../../../components/Tamed";
 import { isActiveRoom, isRoom } from "../../../lib/types";
 import useRoom from "../../../lib/useRoom";
+import { Text } from "react-native";
+import { View } from "../../../components/Tamed";
 
 export default function RoomView() {
   const search = useLocalSearchParams();
   const roomId = search.id as string;
   const room = useRoom(roomId);
 
-  if (isActiveRoom(room)) {
-    return <ActiveRoomView roomId={room.id} />;
-  } else if (isRoom(room)) {
-    //return <RoomHistory room={room} />;
-    return <Text>TODO</Text>
-  }
+  console.log("room", room);
+  console.log("isActiveRoom(room)", isActiveRoom(room));
+  console.log("isRoom(room)", isRoom(room));
+
+  return (
+    <View>
+      {isActiveRoom(room) && <ActiveRoomView room={room} />}
+      {isRoom(room) && <View></View>}
+      {!room && (
+        <Text>
+          Vous semblez perdu, la salle à laquelle vous essayez d'accéder
+          n'existe pas, ou vous n'êtes pas autorisé à y accéder.
+        </Text>
+      )}
+    </View>
+  );
 }
