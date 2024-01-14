4fab5568e524541edb0ca8fcf97a46519113f722
MAXOUXAX <24844231+MAXOUXAX@users.noreply.github.com>
2024-01-14 22:13:18 +0100

    feat(frontend): added Outfit font

--- expo/app/_layout.tsx
+++ expo/app/_layout.tsx
@@ -18,7 +18,15 @@ SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const [loaded, error] = useFonts({
-    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
+    "Outfit-Thin": require("../assets/fonts/outfit/Outfit-Thin.ttf"),
+    "Outfit-ExtraLight": require("../assets/fonts/outfit/Outfit-ExtraLight.ttf"),
+    "Outfit-Light": require("../assets/fonts/outfit/Outfit-Light.ttf"),
+    "Outfit-Medium": require("../assets/fonts/outfit/Outfit-Medium.ttf"),
+    "Outfit-Regular": require("../assets/fonts/outfit/Outfit-Regular.ttf"),
+    "Outfit-SemiBold": require("../assets/fonts/outfit/Outfit-SemiBold.ttf"),
+    "Outfit-Bold": require("../assets/fonts/outfit/Outfit-Bold.ttf"),
+    "Outfit-ExtraBold": require("../assets/fonts/outfit/Outfit-ExtraBold.ttf"),
+    "Outfit-Black": require("../assets/fonts/outfit/Outfit-Black.ttf"),
     ...FontAwesome.font,
    });
 
