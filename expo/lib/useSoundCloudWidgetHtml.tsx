bfe977ab0a9c9dc5b6961e86e0ab8d7636f1d2f6
MAXOUXAX <24844231+MAXOUXAX@users.noreply.github.com>
2024-01-14 13:58:43 +0100

    feat: created a function that returns an HTML web page that handles communication with the SoundCloud widget

---
+++ expo/lib/useSoundCloudWidgetHtml.tsx
@@ -0,0 +1,94 @@
+export default function useSoundCloudWidgetHtml() {
+  return `
+  <html>
+  <head>
+    <script src="https://w.soundcloud.com/player/api.js"></script>
+    <script>
+      const isPlaying = async () => {
+        const widget = SC.Widget(document.querySelector('iframe'));
+        return await new Promise((resolve, reject) => {
+          widget.isPaused((isPaused) => {
+            resolve(!isPaused);
+          });
+        });
+      }
+    
+      // Fetch the current position in the music
+      async function getPosition() {
+        const widget = SC.Widget(document.querySelector('iframe'));
+        return await new Promise((resolve, reject) => {
+          widget.getPosition((position) => {
+            resolve(position);
+          });
+        });
+      }
+      window.onmessage = async function(event) {
+        const eventName = event.data.command;
+        if (eventName === 'playMusic') {
+          const widget = SC.Widget(document.querySelector('iframe'));
+          const { options, url } = event.data.data;
+          widget.load(
+            url,
+            options,
+          );
+        } else if (eventName === 'play') {
+          const widget = SC.Widget(document.querySelector('iframe'));
+          widget.play();
+        } else if (eventName === 'pause') {
+          const widget = SC.Widget(document.querySelector('iframe'));
+          widget.pause();
+        } else if (eventName === 'setVolume') {
+          const { volume } = event.data.data;
+          const widget = SC.Widget(document.querySelector('iframe'));
+          widget.setVolume(volume);
+        } else if (eventName === 'seekTo') {
+          const { position } = event.data.data;
+          const widget = SC.Widget(document.querySelector('iframe'));
+          widget.seekTo(position);
+        } else if (eventName === "fetchCurrent") {
+          const isCurrentlyPlaying = await isPlaying();
+          const position = await getPosition();
+          const widget = SC.Widget(document.querySelector('iframe'));
+          
+          const playingMusic = await new Promise((resolve, reject) => {
+            widget.getCurrentSound((currentSound) => {
+              if (!currentSound) return resolve(null);
+      
+              const playingMusic = {
+                title: currentSound.title.split(" - ")[1],
+                artwork: currentSound.artwork_url.replace("large", "t500x500"),
+                artists: currentSound.title
+                  .split(" - ")[0]
+                  .split(", ")
+                  .map((name) => ({
+                    name,
+                    id: name,
+                  })),
+                duration_ms: currentSound.duration,
+                progress_ms: position,
+                is_playing: isCurrentlyPlaying,
+              };
+      
+              resolve(playingMusic);
+            });
+          });
+
+          window.parent.postMessage(
+            {
+              command: "currentMusic",
+              data: {
+                playingMusic,
+              },
+            },
+            "*"
+          );
+        }
+      }
+    </script>
+  </head>
+  <body>
+    <iframe src="https://w.soundcloud.com/player/?url=https://api.soundcloud.com/" scrolling="no" allow="autoplay"></iframe>
+  </body>
+  </html>
+`;
+}
