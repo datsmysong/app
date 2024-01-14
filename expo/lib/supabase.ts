98848224f4e3b5f0c5f3d7bdd7b08365d38811e8
GaspardBBY <96123850+GaspardBBY@users.noreply.github.com>
2024-01-14 17:19:09 +0100

    chore(expo): added env variable to expo project with skeleton

--- expo/lib/supabase.ts
+++ expo/lib/supabase.ts
@@ -7,7 +7,7 @@ import { Database } from "../../commons/Database-types";
 // var aesjs = require('aes-js');
 
 // https://github.com/supabase/supabase-js/issues/786
-// Bug ith classical AsyncStorage, so I use this one
+// Bug with classical AsyncStorage, so I use this one
 class SupabaseStorage {
   async getItem(key: string) {
     if (Platform.OS === "web") {
@@ -36,52 +36,11 @@ class SupabaseStorage {
   }
 }
 
-// class LargeSecureStore {
-//   private async _encrypt(key: string, value: string) {
-//     const encryptionKey = crypto.getRandomValues(new Uint8Array(256 / 8));
-
-//     const cipher = new aesjs.ModeOfOperation.ctr(encryptionKey, new aesjs.Counter(1));
-//     const encryptedBytes = cipher.encrypt(aesjs.utils.utf8.toBytes(value));
-
-//     await SecureStore.setItemAsync(key, aesjs.utils.hex.fromBytes(encryptionKey));
-
-//     return aesjs.utils.hex.fromBytes(encryptedBytes);
-//   }
-
-//   private async _decrypt(key: string, value: string) {
-//     const encryptionKeyHex = await SecureStore.getItemAsync(key);
-//     if (!encryptionKeyHex) {
-//       return encryptionKeyHex;
-//     }
-
-//     const cipher = new aesjs.ModeOfOperation.ctr(aesjs.utils.hex.toBytes(encryptionKeyHex), new aesjs.Counter(1));
-//     const decryptedBytes = cipher.decrypt(aesjs.utils.hex.toBytes(value));
-
-//     return aesjs.utils.utf8.fromBytes(decryptedBytes);
-//   }
-
-//   async getItem(key: string) {
-//     const encrypted = await AsyncStorage.getItem(key);
-//     if (!encrypted) { return encrypted; }
-
-//     return await this._decrypt(key, encrypted);
-//   }
-
-//   async removeItem(key: string) {
-//     await AsyncStorage.removeItem(key);
-//     await SecureStore.deleteItemAsync(key);
-//   }
-
-//   async setItem(key: string, value: string) {
-//     const encrypted = await this._encrypt(key, value);
-
-//     await AsyncStorage.setItem(key, encrypted);
-//   }
-// }
-
-const supabaseUrl = "https://ckalsdcwrofxvgxslwiv.supabase.co";
-const supabaseAnonKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNrYWxzZGN3cm9meHZneHNsd2l2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDQ1NzkzNjAsImV4cCI6MjAyMDE1NTM2MH0.m6yeTDM0cb04tn4tna2WcRKZ410gqv6K6KZ0E2GZ8Hs";
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY;
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error("Missing env variables for Supabase");
+}
 
 export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey, {
   auth: {
