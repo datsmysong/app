ee97368dc7b96870c90c99450d0ea268f377aa3c
TheRealSharKzy~ <thomas.boillot1@etu.univ-lorraine.fr>
2024-01-16 10:30:18 +0100

    feat(frontend) : can link his account with his spotify account

---
+++ expo/app/(auth)/connect-with-spotify.tsx
@@ -0,0 +1,15 @@
+import Button from "../../components/Button";
+import { getSpotifyScopes } from "../../constants/Api";
+import { signInWithProvider } from "../../lib/providerMethods";
+
+export default function ConnectWithSpotify() {
+    const handleSignUp = async () => {
+        signInWithProvider({ provider: "spotify", scopes: getSpotifyScopes() });
+    };
+
+    return (
+        <Button prependIcon="music-note" onPress={handleSignUp} block>
+            Rejoindre avec Spotify
+        </Button>
+    );
+}
