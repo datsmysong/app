97c8ca73dfd1bdce5c1746e57d7b54f2d573e732
MAXOUXAX <24844231+MAXOUXAX@users.noreply.github.com>
2024-01-17 22:28:29 +0100

    fix: simplified updateState handler for testing purposes

--- backend/src/websocket/updateState.ts
+++ backend/src/websocket/updateState.ts
@@ -1,21 +1,10 @@
-// 1. Check that the client is the host
-// 2. Update the playback state of the room
-// 3. Send a "stateUpdated" message to all clients in the room
 import { Server, Socket } from "socket.io";
 
-let roomPlaybackState = {};
-
-// 1. Check that the client is the host
 const updateState =
   (io: Server, socket: Socket) => async (message: any, done: any) => {
-    if (socket.id !== message.hostId) {
-      return;
-    }
-
-    // 2. Update the playback state of the room
-    roomPlaybackState = message.state;
-
-    // 3. Send a "stateUpdated" message to all clients in the room
-    io.to(message.roomId).emit("stateUpdated", message.state);
+    console.log("updateState", message);
+    const playingMusic = message.data.playingMusic;
+    
+    socket.emit("stateUpdated", playingMusic);
   };
 export default updateState;
