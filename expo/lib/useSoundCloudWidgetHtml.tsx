83373e6ed8ef4b259ec563cf35cee179d5a1e99a
MAXOUXAX <24844231+MAXOUXAX@users.noreply.github.com>
2024-01-17 22:27:01 +0100

    fix: sending message to window instead of window.parent

--- expo/lib/useSoundCloudWidgetHtml.tsx
+++ expo/lib/useSoundCloudWidgetHtml.tsx
@@ -73,7 +73,7 @@ export default function useSoundCloudWidgetHtml() {
             });
           });
 
-          window.parent.postMessage(
+          window.postMessage(
             {
               command: "currentMusic",
               data: {
