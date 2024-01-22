a543dca75aa5b3887516d2a25707fcd3de358b5e
MAXOUXAX <24844231+MAXOUXAX@users.noreply.github.com>
2024-01-22 15:24:14 +0100

    refactor(backend): formatDuration method now shows short unit letters
    m for minutes, s for seconds, etc

--- backend/src/route/RoomGET.ts
+++ backend/src/route/RoomGET.ts
@@ -14,16 +14,16 @@ export interface QueryParams {
 const formatDuration = (ms: number) => {
   if (ms < 0) ms = -ms;
   const time = {
-    day: Math.floor(ms / 86400000),
-    hour: Math.floor(ms / 3600000) % 24,
-    minute: Math.floor(ms / 60000) % 60,
-    second: Math.floor(ms / 1000) % 60,
-    millisecond: Math.floor(ms) % 1000,
+    d: Math.floor(ms / 86400000),
+    h: Math.floor(ms / 3600000) % 24,
+    m: Math.floor(ms / 60000) % 60,
+    s: Math.floor(ms / 1000) % 60,
+    ms: Math.floor(ms) % 1000,
   };
   return Object.entries(time)
     .filter((val) => val[1] !== 0)
-    .map(([key, val]) => `${val} ${key}${val !== 1 ? "s" : ""}`)
-    .join(", ");
+    .map(([key, val]) => `${val}${key}`)
+    .join(" ");
 };
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -127,10 +127,16 @@ const useProcessRoom = (
 
   const streamingService = room.streaming_services;
   const playedSongs = usePlayedSongs(room.room_history, user);
-  const averageSongDuration = formatDuration(
-    playedSongs.reduce((acc, song) => acc + song.duration, 0) /
-      playedSongs.length
-  );
+
+  let averageSongDuration = "0s";
+  if (playedSongs.length >= 1) {
+    averageSongDuration = formatDuration(
+      (playedSongs.reduce((acc, song) => acc + song.duration, 0) /
+        playedSongs.length) *
+        1000
+    );
+  }
+
   const genresPlayCount = playedSongs
     .map((song) => song.genre)
     .reduce((acc, genre) => {
