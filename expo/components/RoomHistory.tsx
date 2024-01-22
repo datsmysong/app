966cbaa5252a81277ce131d60c686fff66eb0425
MAXOUXAX <24844231+MAXOUXAX@users.noreply.github.com>
2024-01-22 17:06:00 +0100

    feat(frontend): fetching room data from backend and started view

--- expo/components/RoomHistory.tsx
+++ expo/components/RoomHistory.tsx
@@ -1,43 +1,64 @@
-import type { Room } from "commons/database-types-utils";
+import type { ProcessedRoom } from "commons/room-types";
 import { useEffect, useState } from "react";
 import { StyleSheet } from "react-native";
 
+import InfoCard from "./InfoCard";
 import { Text, View } from "./Tamed";
 
 type RoomHistoryProps = {
-  room: Room;
+  roomId: string;
 };
 
-const RoomHistory: React.FC<RoomHistoryProps> = ({ room }) => {
-  const formatter = new Intl.RelativeTimeFormat("fr", {
-    numeric: "auto",
-    style: "long",
-  });
+const RoomHistory: React.FC<RoomHistoryProps> = ({ roomId }) => {
   const [processedRoom, setProcessedRoom] = useState<ProcessedRoom>();
 
   useEffect(() => {
     const fetchProcessedRoomData = async () => {
-      const processedRoomData = await fetch(
-        `http://localhost:3000/rooms/${room.id}`
-      );
-      setProcessedRoom(processedRoomData);
+      const data = await fetch(`http://localhost:3000/rooms/${roomId}`, {
+        method: "GET",
+        credentials: "include",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      const processedRoomData = await data.json();
+      setProcessedRoom(processedRoomData as ProcessedRoom);
     };
     fetchProcessedRoomData();
-  }, [room]);
+  }, [roomId]);
 
   return (
     <View style={styles.header}>
-      <Text>{room.name}</Text>
-      <View style={styles.headerInfos}>
-        <View style={styles.headerLeft}>
-          <Text>{relativeTime}</Text>
-          <Text>
-            {room.participants.length}
-            {room.participants.length > 1 ? "participants" : "participant"}
-          </Text>
-          <Text>{formattedDuration}</Text>
-        </View>
-      </View>
+      {processedRoom && (
+        <>
+          <View style={styles.infoCards}>
+            <InfoCard
+              content={processedRoom.createdAt}
+              description="La date à laquelle s'est déroulée cette salle d'écoute"
+              icon="calendar-today"
+              title="Date"
+            />
+            <InfoCard
+              content={processedRoom.duration}
+              description="La durée de cette salle d'écoute"
+              icon="timer"
+              title="Durée"
+            />
+            <InfoCard
+              content={processedRoom.participants.length + ""}
+              description="Le nombre de participants à cette salle d'écoute"
+              icon="people"
+              title="Participants"
+            />
+          </View>
+          <Text>{processedRoom.name}</Text>
+          <View style={styles.headerInfos}>
+            <View style={styles.headerLeft}>
+              <Text>{JSON.stringify(processedRoom)}</Text>
+            </View>
+          </View>
+        </>
+      )}
     </View>
   );
 };
@@ -45,9 +66,21 @@ const RoomHistory: React.FC<RoomHistoryProps> = ({ room }) => {
 const styles = StyleSheet.create({
   header: {
     padding: 10,
-    backgroundColor: "white",
+    backgroundColor: "transparent",
     borderBottomColor: "#ccc",
     borderBottomWidth: 1,
+  },
+  infoCards: {
+    display: "flex",
+    backgroundColor: "transparent",
+    alignItems: "flex-start",
+    flexDirection: "row",
+    gap: 12,
+    alignSelf: "stretch",
+    overflow: "scroll",
+  },
+  infoCard: {
+    height: "auto",
   },
   headerInfos: {
     flexDirection: "row",
