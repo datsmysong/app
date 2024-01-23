ba4cce08095084e64d7ae3f05b4e571cb7311bf5
ngomesodent <101274479+ngomesodent@users.noreply.github.com>
2024-01-23 10:54:12 +0100

    feat: add business logic to search and join a room

--- expo/app/(tabs)/rooms/join.tsx
+++ expo/app/(tabs)/rooms/join.tsx
@@ -1,28 +1,84 @@
 import { StyleSheet, View, Text } from "react-native";
 import CustomTextInput from "../../../components/CustomTextInput";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { supabase } from "../../../lib/supabase";
+import Button from "../../../components/Button";
+import { router } from "expo-router";
 
 export default function JoinRoom() {
   const [roomCode, setRoomCode] = useState("");
+  const [isTextPresent, setIsTextPresent] = useState(false);
+  const [noRoomFound, setNoRoomFound] = useState(false);
+
+  useEffect(() => {
+    setIsTextPresent(roomCode.length > 0);
+  }, [roomCode]);
+
+  const onSubmitSearchRoom = async (roomCode: string) => {
+    if (!roomCode) {
+      return;
+    }
+
+    const { data: room, error: roomError } = await supabase
+      .from("active_rooms")
+      .select("id")
+      .eq("code", roomCode)
+      .single();
+
+    if (roomError) {
+      setNoRoomFound(true);
+      return;
+    }
+
+    router.push(`/rooms/${room.id}`);
+  };
 
   return (
-    <View style={styles.mainContainer}>
-      <CustomTextInput
-        placeholder="Code de la salle"
-        value={roomCode}
-        onChangeText={setRoomCode}
-      />
-    </View>
+    <>
+      <View style={styles.mainContainer}>
+        <CustomTextInput
+          placeholder="Code de la salle"
+          value={roomCode}
+          onChangeText={setRoomCode}
+          onSubmitEditing={() => onSubmitSearchRoom(roomCode)}
+        />
+        <Button
+          type="filled"
+          block
+          disabled={!isTextPresent}
+          prependIcon="search"
+          onPress={() => onSubmitSearchRoom(roomCode)}
+        >
+          Rechercher
+        </Button>
+      </View>
+      {noRoomFound && (
+        <View style={styles.messageContainer}>
+          <Text style={styles.message}>
+            Aucune salle n'a été trouvée avec ce code.
+          </Text>
+        </View>
+      )}
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   mainContainer: {
     display: "flex",
-    padding: 24,
+    paddingHorizontal: 24,
+    paddingTop: 24,
     flexDirection: "column",
-    gap: 48,
-    flex: 1,
+    gap: 24,
     alignSelf: "stretch",
+  },
+  messageContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  message: {
+    fontSize: 16,
+    textAlign: "center",
   },
 });
