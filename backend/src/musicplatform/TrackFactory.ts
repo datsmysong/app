7499e81bada08a86cf2d282b364869d82b8fbe52
Hugo Demange <hugo.demange8@etu.univ-lorraine.fr>
2024-01-23 15:25:02 +0100

    refactor : clean useless comments through bobby review

--- backend/src/musicplatform/TrackFactory.ts
+++ backend/src/musicplatform/TrackFactory.ts
@@ -34,14 +34,3 @@ export default class TrackFactory {
     return null;
   }
 }
-
-// // DEBUG
-// let track = new TrackFabrique(new Spotify()).fromUrl(new URL("https://open.spotify.com/intl-fr/track/4OUTQBwLBaTIUcgdI5PPt7?si=3aac1a9bcf3d4eac"));
-// // track.getData().then(console.log)
-// track.getData().then(d => {
-//     d.duration = Math.round(d.durationMs / 60) / 1000;
-//     let durationRawObj: date = new_date(d_durationMs);
-//     d.durationObj = {hours: durationRawObj.getUTCHours(), minutes: durationRawObj.getUTCMinutes(), seconds: durationRawObj.getUTCSeconds()};
-//
-//     return d;
-// }).then(console.log)
