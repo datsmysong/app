dbb64077f0eb2115674f25728405a09ea3ad6c44
GaspardBBY <96123850+GaspardBBY@users.noreply.github.com>
2024-01-24 17:25:56 +0100

    feat/rooms-links (#45)
    * feat: add two buttons to create and join a room
    
    * feat: add deep links setup (Android only)
    
    * refactor: change rooms page implementation
    
    * feat: add types needed in the app
    
    * fix: improve UI
    
    * fix: change deep link that weren't working
    
    * refactor: change the way we pass labels to Button component and Link refs warning
    
    * feat: beginning of opening a opening a web page when clicking the invitation link
    
    * chore: improve style
    
    * feat: add a join page
    
    * feat: add invitation link that works on both web and mobile (dev only)
    
    * refactor: stop hardcoding roomCode and get it by the params instead
    
    * feat: add deep link on open in app button (doesn't work yet)
    
    * chore(expo): ui to join room
    
    * fix: typo
    
    * chore: improve rooms page style
    
    * chore: change room page style to fit website mockup
    
    * chore: improve join page style
    
    * refactor: remove unused imports
    
    * build: add expo device for device type check instead of Platform.OS
    
    * feat: add the handle of incoming links and change pages display based on the case (partial)
    
    * feat: add verification of a user's connection and participation in a room
    
    * feat: add the user in the room when clicking the invitation link (not finished yet)
    
    * fix: change room_users.room_id type that matches the active_room.id one
    
    * fix: more accurate page and database behavior
    
    * fix: rebase problem
    
    * fix: fix supabase insertion and fetch errors
    
    * fix: fix mobile users that are instantly redirected to the room page
    
    * refactor: better error handling and remove useless state
    
    * fix: remove useless Views that prevented Text to be displayed on mobile
    
    * feat: add feedback on copying the room link
    
    * refactor: remove useless useEffect and function
    
    * refactor: better error handling
    
    * fix: change the way deep link is created and shared
    
    * fix: add rejected promise handling
    
    * fix: change state of the share button after 5 seconds for better UX
    
    * refactor: replace userProfileId variable by state and add parameter to functions that use it
    
    * fix: do not redirect to the active room page when addUserToRoom returns an error
    
    * refactor: remove unused import
    
    * refactor: remove unused import
    
    * chore: remove already merged file
    
    * refactor: better destructuration of the result
    
    * refactor(frontend): room view share button now works properly
    
    * fix(frontend): updated route to use the UUID of the room instead of the code
    
    * refactor(frontend): join page should now work as intended
    
    If the page is accessed within the app, then it should automatically make the current user join the room and redirect him to the proper page.
    If the page is accessed outside of the app, then it displays a view that depends on whether the user is logged in or not, whether he's already a participant of the room, etc
    
    * Update expo/app/join/[roomCode].tsx
    
    * fix(frontend): link generated was wrong when inside of Expo Go
    
    * fix(frontend): wrong state when isParticipant wasn't fetched
    
    * fix: a problem with the select with single
    
    ---------
    
    Co-authored-by: ngomesodent <101274479+ngomesodent@users.noreply.github.com>
    Co-authored-by: MAXOUXAX <24844231+MAXOUXAX@users.noreply.github.com>

--- expo/app/(tabs)/rooms/index.tsx
+++ expo/app/(tabs)/rooms/index.tsx
@@ -1,12 +1,37 @@
-
 import React from "react";
-import { View } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import Button from "../../../components/Button";
 
-export default function TabsRooms() {
+export default function RoomsPage() {
+  const roomId = "956992d0-f039-47d1-9475-ccacf10104d7";
+
   return (
-    <View>
-      <Button href="/rooms/create">Créer une salle</Button>
+    <View style={styles.headerContainer}>
+      <Text style={styles.title}>Salles d'écoute</Text>
+      <View style={styles.buttonContainer}>
+        <Button block href={"/rooms/create"}>
+          Créer une salle
+        </Button>
+        <Button block type="outline" href={`/rooms/${roomId}`}>
+          Rejoindre une salle
+        </Button>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  headerContainer: {
+    flex: 1,
+    marginHorizontal: 24,
+    marginVertical: 21,
+    gap: 36,
+  },
+  buttonContainer: {
+    gap: 8,
+  },
+  title: {
+    fontSize: 32,
+    fontWeight: "bold",
+  },
+});
---
+++ expo/app/join/[roomCode].tsx
@@ -0,0 +1,245 @@
+import { User } from "@supabase/supabase-js";
+import { UserProfile } from "commons/database-types-utils";
+import * as Linking from "expo-linking";
+import { router, useLocalSearchParams } from "expo-router";
+import { useEffect, useState } from "react";
+import { Platform, StyleSheet, Text, View } from "react-native";
+
+import Alert from "../../components/Alert";
+import Button from "../../components/Button";
+import { supabase } from "../../lib/supabase";
+import useSupabaseUser from "../../lib/useSupabaseUser";
+
+export default function JoinPage() {
+  const { roomCode } = useLocalSearchParams();
+  const isInsideApplication = Platform.OS !== "web";
+
+  const [user, setUser] = useState<User | null>();
+  const [userProfile, setUserProfile] = useState<UserProfile>();
+  const [isParticipant, setIsParticipant] = useState<boolean>();
+  const [roomId, setRoomId] = useState<string>("");
+  const currentPageLink = Linking.useURL();
+
+  /**
+   * This effect fetches the user and the room id based on the room code given in the route
+   * during the first render of the page.
+   */
+  useEffect(() => {
+    const fetchUser = async () => {
+      // eslint-disable-next-line react-hooks/rules-of-hooks
+      const user = await useSupabaseUser();
+      setUser(user);
+    };
+    const fetchRoomId = async () => {
+      const { data } = await supabase
+        .from("active_rooms")
+        .select("id")
+        .eq("code", roomCode)
+        .single();
+
+      if (!data) {
+        Alert.alert("Aucune salle d'écoute n'a été trouvée avec ce code.");
+        return;
+      }
+      setRoomId(data.id);
+    };
+
+    fetchUser();
+    fetchRoomId();
+  }, []);
+
+  /**
+   * This effect fetches the user profile based on the user id.
+   */
+  useEffect(() => {
+    if (!user) return;
+
+    const fetchData = async () => {
+      const { data: userProfile, error } = await supabase
+        .from("user_profile")
+        .select("*")
+        .eq("account_id", user.id)
+        .single();
+      if (error) {
+        Alert.alert(
+          "Une erreur est survenue lors de la récupération du profil"
+        );
+        return;
+      }
+      setUserProfile(userProfile);
+    };
+
+    fetchData();
+  }, [user]);
+
+  /**
+   * This effect fetches the participant based on the user profile and the room id.
+   */
+  useEffect(() => {
+    if (!userProfile || !roomId) return;
+
+    const fetchParticipant = async () => {
+      const { data } = await supabase
+        .from("room_users")
+        .select("*")
+        .eq("profile_id", userProfile.user_profile_id)
+        .eq("room_id", roomId);
+
+      setIsParticipant((data?.length ?? 0) > 0);
+    };
+
+    fetchParticipant();
+  }, [userProfile, roomId]);
+
+  /**
+   * This automatically joins the user to the room if he is on mobile and not a participant.
+   * When this page is opened inside the app, we don't want to show the choice to join the room.
+   * We instead join the room automatically and redirect the user to the room page.
+   */
+  useEffect(() => {
+    if (isParticipant === undefined) return;
+    if (!userProfile || !isInsideApplication || !roomId) return;
+
+    if (!isParticipant) {
+      joinRoom().then((r) => {
+        if (r?.error) {
+          console.log(r?.error);
+          return Alert.alert("Impossible de rejoindre la salle d'écoute");
+        }
+      });
+    }
+    router.replace(`rooms/${roomId}`);
+  }, [userProfile, isInsideApplication, roomId, isParticipant]);
+
+  /**
+   * Joins a room by inserting the user's profile into the room_users table.
+   * If the user is already a participant, it will act like he joined the room.
+   */
+  const joinRoom = async () => {
+    if (!userProfile) return { error: "Unauthorized" };
+    if (!roomId) return { error: "Unknown room" };
+
+    if (isParticipant) return { error: null };
+    console.log("user", userProfile);
+
+    const { error: roomUsersError } = await supabase.from("room_users").insert({
+      room_id: roomId,
+      profile_id: userProfile.user_profile_id,
+    });
+
+    return { error: roomUsersError };
+  };
+
+  /**
+   * Returns the app link based on the current environment and room code.
+   * On production, the app link is a deep link to the app (datsmysong://)
+   * On development, the app link is a deep link to the Expo Go app (exp://)
+   * Currently, this doesn't support native development builds.
+   * @returns The app link.
+   */
+  function getAppLink(): string {
+    const production = process.env.NODE_ENV === "production";
+    if (production) {
+      return `datsmysong://join/${roomCode}`;
+    } else {
+      if (!currentPageLink) return "";
+
+      const host = currentPageLink.split("/").slice(2, 3).join("/");
+      return `exp://${host}/--/join/${roomCode}`;
+    }
+  }
+
+  /**
+   * Handles the action when the user continues on the website.
+   * If the user is already a participant, it replaces the current route with the room route.
+   * Otherwise, it joins the room and replaces the current route with the room route.
+   */
+  async function handleContinueOnWebsite() {
+    if (isParticipant) return router.replace(`rooms/${roomId}`);
+
+    const { error } = await joinRoom();
+    if (error)
+      return Alert.alert("Impossible de rejoindre la salle d'écoute: " + error);
+
+    router.replace(`rooms/${roomId}`);
+  }
+
+  function handleAnonymousJoin() {
+    return Alert.alert("Fonctionnalité non implémentée");
+  }
+
+  return (
+    <View style={styles.choiceContainer}>
+      {roomCode && (
+        <>
+          {isInsideApplication && (
+            <Text style={styles.title}>Redirection en cours...</Text>
+          )}
+          {!isInsideApplication && (
+            <>
+              {isParticipant && (
+                <>
+                  <Text style={styles.title}>
+                    Vous avez déjà rejoint la salle d'écoute "{roomCode}" !
+                  </Text>
+                  <Text style={styles.title}>
+                    Comment souhaitez-vous continuer ?
+                  </Text>
+                </>
+              )}
+              {!isParticipant && (
+                <Text style={styles.title}>
+                  Vous êtes sur le point de rejoindre la salle d'écoute "
+                  {roomCode}"
+                </Text>
+              )}
+
+              <View style={styles.buttonContainer}>
+                <Button block type="filled" href={getAppLink()}>
+                  Ouvrir dans l'application
+                </Button>
+                {!userProfile && (
+                  <Button block type="outline" onPress={handleAnonymousJoin}>
+                    Continuer en tant qu'invité
+                  </Button>
+                )}
+                {userProfile && (
+                  <Button
+                    block
+                    type="outline"
+                    onPress={handleContinueOnWebsite}
+                  >
+                    Continuer sur le site
+                  </Button>
+                )}
+              </View>
+            </>
+          )}
+        </>
+      )}
+      {!roomCode && (
+        <Text style={styles.title}>
+          Aucun code de salle d'écoute n'a été retourné
+        </Text>
+      )}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  choiceContainer: {
+    flex: 1,
+    justifyContent: "center",
+    marginHorizontal: 24,
+    gap: 10,
+  },
+  buttonContainer: {
+    gap: 8,
+  },
+  title: {
+    marginBottom: 20,
+    fontSize: 28,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+});
