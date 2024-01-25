f9e154b39b0ffb877633ce51faa13e60797e790c
Hugo Demange <hugo.demange8@etu.univ-lorraine.fr>
2024-01-25 09:58:19 +0100

    refactor(frontend) : Maxence's review : define socket io endpoint to backend through already coded function

--- expo/lib/socketio.ts
+++ expo/lib/socketio.ts
@@ -1,17 +1,13 @@
-import { Manager, Socket } from "socket.io-client"; // https://docs.expo.dev/guides/environment-variables/
+import { Manager, Socket } from "socket.io-client";
 
-// https://docs.expo.dev/guides/environment-variables/
-const ENDPOINT = process.env.EXPO_PUBLIC_BACKEND_API ?? "";
-if (!ENDPOINT) {
-  throw new Error("le endpoint de communication socket.io n'est pas défini");
-}
+import { getApiUrl } from "./apiUrl";
 
 export default class SocketIo {
   private static singleton: SocketIo;
   private readonly iomanager: Manager;
 
   private constructor() {
-    this.iomanager = new Manager(new URL(ENDPOINT));
+    this.iomanager = new Manager(new URL(getApiUrl()));
   }
 
   static getInstance(): SocketIo {
