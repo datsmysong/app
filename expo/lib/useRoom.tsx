1022e8dbb177e435c903d651ad60d6a5bf1ed0a9
MAXOUXAX <24844231+MAXOUXAX@users.noreply.github.com>
2024-02-01 11:45:16 +0100

    fix: supabase query side effect

--- expo/lib/useRoom.tsx
+++ expo/lib/useRoom.tsx
@@ -12,9 +12,15 @@ export type ActiveRoom = QueryData<typeof fetchData>[0];
 export default function useRoom(roomId: string) {
   const [room, setRoom] = useState<ActiveRoom | null>(null);
 
+  /** This is temporary, since the query is duplicated */
+  const conditionalFetchData = supabase
+    .from("rooms")
+    .select("*, streaming_services(*)")
+    .eq("is_active", true);
+
   useEffect(() => {
     async function fetchRoom() {
-      const { data } = await fetchData.eq("id", roomId).single();
+      const { data } = await conditionalFetchData.eq("id", roomId).single();
       setRoom(data);
     }
     fetchRoom();
