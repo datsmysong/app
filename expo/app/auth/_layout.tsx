6dede67aba67af1a635c54c8bd50e9ee60b6085d
GaspardBBY <96123850+GaspardBBY@users.noreply.github.com>
2024-02-16 18:19:44 +0100

    fix: prevent token refetch after disconnecting from provider

--- expo/app/auth/_layout.tsx
+++ expo/app/auth/_layout.tsx
@@ -1,10 +1,37 @@
-import { Redirect, Stack } from "expo-router";
+import { Redirect, Stack, useNavigation, useRouter } from "expo-router";
+import { Alert, Linking } from "react-native";
 
-import { View, Text } from "../../components/Themed";
+import { Text, View } from "../../components/Themed";
+import { supabase } from "../../lib/supabase";
 import { useSupabaseUserHook } from "../../lib/useSupabaseUser";
 
 export default function TabLayout() {
   const user = useSupabaseUserHook();
+  const url = Linking.getInitialURL();
+
+  const router = useRouter();
+  const navigation = useNavigation();
+
+  url.then(async (url) => {
+    if (!url) return;
+    const refresh_token = url.split("#refresh_token=")[1];
+    if (refresh_token) {
+      const { error } = await supabase.auth.refreshSession({
+        refresh_token,
+      });
+      // clear the url history to avoid the user to go back to the login page & delete the refresh_token from the url
+      // router.replace clear not the history
+      const state = navigation.getState();
+      navigation.reset({
+        ...state,
+        routes: state.routes.map((route) => ({ ...route, state: undefined })),
+      });
+      if (error) {
+        router.replace("/auth");
+      }
+      router.push("/(tabs)");
+    }
+  });
 
   if (user === undefined)
     return (
