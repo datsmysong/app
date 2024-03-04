4be184c5073af2abd7a03d44d79c7244906fd36b
GaspardBBY <96123850+GaspardBBY@users.noreply.github.com>
2024-03-04 17:29:58 +0100

    fix: wrong layout

--- expo/app/(tabs)/rooms/_layout.tsx
+++ expo/app/(tabs)/rooms/_layout.tsx
@@ -10,10 +10,6 @@ export default function RoomsTabLayout() {
         options={{ presentation: "modal", title: "Création d'une salle" }}
       />
       <Stack.Screen name="[id]" options={{ headerShown: false }} />
-      <Stack.Screen
-        name="[id]/add"
-        options={{ title: "Ajouter une musique" }}
-      />
     </Stack>
   );
 }
