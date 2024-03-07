cfadc7d8ef8113c2229eab898deb8976ba3e45f6
GaspardBBY <96123850+GaspardBBY@users.noreply.github.com>
2024-03-07 14:56:16 +0100

    fix/production-login-bug-2 (#123)
    * fix: try close webbroser on prod
    
    * chore: moved fix on a single page compared to layout

--- expo/app/(tabs)/index.tsx
+++ expo/app/(tabs)/index.tsx
@@ -1,6 +1,8 @@
+import * as WebBrowser from "expo-web-browser";
 import { Text } from "react-native";
 
 export default function TabOneScreen() {
+  WebBrowser.maybeCompleteAuthSession();
   return (
     <>
       <Text>Index</Text>
