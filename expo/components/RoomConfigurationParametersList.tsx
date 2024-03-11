7de2b997b984fb80075e510b185dbe6bc3fc3ca7
TheRealSharKzy~ <thomas.boillot1@etu.univ-lorraine.fr>
2024-03-11 17:28:06 +0100

    refactor(frontend) : use the same component for the parameters lists on the create room page and the room configuration page

--- expo/components/RoomConfigurationParametersList.tsx
+++ expo/components/RoomConfigurationParametersList.tsx
@@ -5,10 +5,8 @@ import { StyleSheet } from "react-native";
 
 import Alert from "./Alert";
 import Button from "./Button";
-import CustomCheckbox from "./CustomCheckbox";
-import CustomSlider from "./CustomSlider";
-import CustomTextInput from "./CustomTextInput";
-import { Text, View } from "./Themed";
+import ParametersList from "./ParametersList";
+import { View } from "./Themed";
 import { getApiUrl } from "../lib/apiUrl";
 import { supabase } from "../lib/supabase";
 
@@ -21,13 +19,10 @@ export default function RoomConfigurationParametersList({
 }: ParametersListProps) {
   const baseUrl = getApiUrl();
   const [roomConfigurationId, setRoomConfigurationId] = useState<string>("");
-  const [sliderParticipantValue, setSliderParticipantValue] = useState(10);
-  const [canBeAnonymous, setCanBeAnonymous] = useState(false);
   const [canSkip, setCanSkip] = useState(true);
   const [sliderPercentageValue, setSliderPercentageValue] = useState(70);
   const [maxMusicDuration, setMaxMusicDuration] = useState("150");
   const [maxMusicPerUser, setMaxMusicPerUser] = useState("5");
-  const thumbImage = require("../assets/images/SliderElipse.svg");
 
   // To get current room configuration id
   useEffect(() => {
@@ -103,80 +98,16 @@ export default function RoomConfigurationParametersList({
 
   return (
     <View style={styles.page}>
-      <Text style={styles.title}>Paramètres de la salle</Text>
-      <View
-        style={{
-          gap: 16,
-        }}
-      >
-        <View style={styles.slider}>
-          <Text style={styles.labelText}>
-            Limite de participants <Text style={{ color: "red" }}>*</Text>
-          </Text>
-          <CustomSlider
-            maximumValue={20}
-            minimumValue={2}
-            maximumTrackTintColor="#CCCCCC"
-            minimumTrackTintColor="#1A1A1A"
-            // Doesn't change anything, don't really understand why, but I'll need to find a
-            // solution to this later
-            value={sliderParticipantValue}
-            setValue={(value) => setSliderParticipantValue(value)}
-            // The column is not on the table room_configuration
-            // onSlidingComplete={handleSave}
-            step={1}
-          />
-        </View>
-        <CustomCheckbox
-          value={canBeAnonymous}
-          setValue={setCanBeAnonymous}
-          label="Autoriser les utilisateurs anonymes"
-        />
-        <View style={styles.separator} />
-        <CustomCheckbox
-          value={canSkip}
-          setValue={setCanSkip}
-          label="Activer le vote skipping"
-        />
-        <View style={styles.slider}>
-          <Text style={styles.labelText}>
-            Pourcentage nécessaire <Text style={{ color: "red" }}>*</Text>
-          </Text>
-          <CustomSlider
-            maximumValue={100}
-            minimumValue={1}
-            maximumTrackTintColor="#CCCCCC"
-            minimumTrackTintColor={canSkip ? "#1A1A1A" : "grey"}
-            thumbImage={thumbImage}
-            value={sliderPercentageValue}
-            setValue={(value) => setSliderPercentageValue(value)}
-            step={5}
-            disabled={!canSkip}
-          />
-        </View>
-        <View style={styles.separator} />
-        <View style={styles.inputLayout}>
-          <Text style={styles.labelText}>
-            Durée maximale d'une musique <Text style={{ color: "red" }}>*</Text>
-          </Text>
-          <CustomTextInput
-            value={maxMusicDuration}
-            onChangeText={setMaxMusicDuration}
-            style={styles.input}
-          />
-        </View>
-        <View style={styles.inputLayout}>
-          <Text style={styles.labelText}>
-            Nombre de musiques maximal par participant{" "}
-            <Text style={{ color: "red" }}>*</Text>
-          </Text>
-          <CustomTextInput
-            value={maxMusicPerUser}
-            onChangeText={setMaxMusicPerUser}
-            style={styles.input}
-          />
-        </View>
-      </View>
+      <ParametersList
+        percentageVoteToSkipAMusic={sliderPercentageValue}
+        setPercentageVote={setSliderPercentageValue}
+        maxMusicPerUser={maxMusicPerUser}
+        setMaxMusicPerUser={setMaxMusicPerUser}
+        maxMusicDuration={maxMusicDuration}
+        setMaxMusicDuration={setMaxMusicDuration}
+        canSkip={canSkip}
+        setCanSkip={setCanSkip}
+      />
       <Button type="filled" onPress={handleSave} block>
         Sauvegarder
       </Button>
@@ -194,78 +125,5 @@ const styles = StyleSheet.create({
     gap: 30,
     paddingVertical: 20,
     flex: 1,
-  },
-  title: {
-    color: "#000",
-    fontFamily: "Outfit-Regular",
-    fontSize: 24,
-    fontStyle: "normal",
-    fontWeight: "700",
-    letterSpacing: 0.48,
-    padding: 10,
-  },
-  slider: {
-    justifyContent: "center",
-    marginLeft: 10,
-    flex: 1,
-  },
-
-  sliderBar: {
-    display: "flex",
-    justifyContent: "space-between",
-    alignItems: "center",
-    alignSelf: "stretch",
-  },
-
-  sliderDuration: {
-    display: "flex",
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-    alignSelf: "stretch",
-  },
-
-  labelText: {
-    fontFamily: "Outfit-Bold",
-    fontSize: 17,
-    fontStyle: "normal",
-  },
-  separator: {
-    height: 2,
-    width: "80%",
-    backgroundColor: "grey",
-    marginLeft: "10%",
-    marginRight: "10%",
-  },
-
-  inputLayout: {
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "center",
-    alignItems: "flex-start",
-    alignSelf: "stretch",
-    gap: 8,
-  },
-  input: {
-    color: "grey",
-    fontSize: 14,
-    width: "100%",
-  },
-  thumbLabel: {
-    position: "absolute",
-    bottom: "100%",
-    borderRadius: 16,
-    width: "10%",
-    height: 19,
-    backgroundColor: "#1A1A1A",
-    color: "#FFF",
-    textAlign: "center",
-    marginBottom: 2,
-  },
-
-  sliderPolygon: {
-    width: "3%",
-    height: 8,
-    flexShrink: 0,
   },
 });
