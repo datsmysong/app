a13f0fd61945e19fcc4ee767d91765dbaad698ba
ngomesodent <101274479+ngomesodent@users.noreply.github.com>
2024-03-22 14:28:28 +0100

    fix: send userProfileId in queue:update

--- expo/components/ActiveRoomView.tsx
+++ expo/components/ActiveRoomView.tsx
@@ -93,8 +93,8 @@ const ActiveRoomView: React.FC<ActiveRoomViewProps> = ({ room }) => {
       setLiveRoom(data);
     });
 
-    socket.on("queue:update", (data: RoomJSON) => {
-      setLiveRoom(data);
+    socket.on("queue:update", (room: RoomJSON, userProfileId: string) => {
+      setLiveRoom(room);
     });
   }, [socket]);
 
