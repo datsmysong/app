9c43ae0ef97d24a9b39a9b4e6b3f5947735c8659
MAXOUXAX <24844231+MAXOUXAX@users.noreply.github.com>
2024-04-04 19:13:25 +0200

    feat: home page header and beginning of actual tab content

--- expo/app/(tabs)/index.tsx
+++ expo/app/(tabs)/index.tsx
@@ -1,12 +1,76 @@
-import * as WebBrowser from "expo-web-browser";
+// import * as WebBrowser from "expo-web-browser";
+import ClockCounterClockwise from "phosphor-react-native/src/icons/ClockCounterClockwise";
 import React from "react";
-import { Text } from "react-native";
+import { View as NativeView, ScrollView } from "react-native";
 
-export default function TabOneScreen() {
-  WebBrowser.maybeCompleteAuthSession();
+import Button from "../../components/Button";
+import { View } from "../../components/Themed";
+import UserRoomHistory from "../../components/UserRoomHistory";
+import H1 from "../../components/text/H1";
+import H2 from "../../components/text/H2";
+import { useUserProfile } from "../../lib/userProfile";
+
+export default function HomeTab() {
+  // This shouldn't be needed, but I'm leaving this here just in case that breaks something
+  // WebBrowser.maybeCompleteAuthSession();
+
   return (
-    <>
-      <Text>Index</Text>
-    </>
+    <ScrollView contentContainerStyle={{ gap: 36, padding: 24 }}>
+      <View
+        style={{
+          gap: 12,
+          justifyContent: "flex-start",
+          alignItems: "flex-start",
+        }}
+      >
+        <View
+          style={{
+            gap: 12,
+            flexDirection: "row",
+            justifyContent: "center",
+            alignItems: "flex-start",
+          }}
+        >
+          <ClockCounterClockwise />
+          <H2>Votre dernière salle</H2>
+        </View>
+        <UserRoomHistory />
+      </View>
+    </ScrollView>
+  );
+}
+
+export function HomeTabHeader() {
+  const userProfile = useUserProfile();
+
+  return (
+    <NativeView
+      style={{
+        flexDirection: "column",
+        backgroundColor: "#E6E6E6",
+        flex: 1,
+        padding: 24,
+        gap: 36,
+      }}
+    >
+      <NativeView
+        style={{
+          flexDirection: "row",
+          justifyContent: "space-between",
+          alignItems: "center",
+          alignContent: "center",
+        }}
+      >
+        <H1>Salut {userProfile?.username ?? ""} 👋</H1>
+      </NativeView>
+      <NativeView style={{ flex: 1, gap: 8, paddingVertical: 12 }}>
+        <Button block href="/rooms/create">
+          Créer une salle
+        </Button>
+        <Button block type="outline" href="/rooms/join">
+          Rejoindre une salle
+        </Button>
+      </NativeView>
+    </NativeView>
   );
 }
