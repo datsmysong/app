ee9205163a0eb75c783a91e3d09aa1a0b24f4408
GaspardBBY <96123850+GaspardBBY@users.noreply.github.com>
2024-04-04 17:48:30 +0200

    feat: added Friend header

--- expo/app/(tabs)/Friends.tsx
+++ expo/app/(tabs)/Friends.tsx
@@ -1,10 +1,30 @@
+import Hammer from "phosphor-react-native/src/icons/Hammer";
 import React from "react";
-import { Text, View } from "react-native";
+import { View } from "react-native";
+
+import { Text } from "../../components/Themed";
 
 export default function TabsFriends() {
   return (
-    <View>
-      <Text>Friends</Text>
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+        padding: 32,
+      }}
+    >
+      <Hammer size={32} weight="fill" color="black" />
+      <Text
+        style={{
+          fontSize: 16,
+          textAlign: "center",
+          fontFamily: "Outfit-Bold",
+        }}
+      >
+        Cette page est en cours de développement. Merci de revenir plus tard.
+      </Text>
     </View>
   );
 }
