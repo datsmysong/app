6b2d8cc6ceeb882689ee45cdef367c25dc4da105
GaspardBBY <96123850+GaspardBBY@users.noreply.github.com>
2024-04-05 22:03:45 +0200

    feat: ask-name page lifting

--- expo/app/ask-name.tsx
+++ expo/app/ask-name.tsx
@@ -1,91 +1,97 @@
-import { router } from "expo-router";
-import { useState } from "react";
-import { StyleSheet, Text, TextInput, View } from "react-native";
+import { useRouter } from "expo-router";
+import { useForm } from "react-hook-form";
 import { Screen } from "react-native-screens";
 
-import Alert from "../components/Alert";
 import Button from "../components/Button";
+import ControlledInput from "../components/ControlledInput";
+import { Text } from "../components/Themed";
+import Font from "../constants/Font";
 import { SupabaseErrorCode } from "../constants/SupabaseErrorCode";
+import { usernameRules } from "../lib/inputRestriction";
 import { supabase } from "../lib/supabase";
-import useSupabaseUser from "../lib/useSupabaseUser";
+import { useSupabaseUserHook } from "../lib/useSupabaseUser";
+
+type UsernameForm = {
+  username: string;
+};
 
 export default function AskName() {
-  const [username, setUsername] = useState("");
+  const user = useSupabaseUserHook();
+  const router = useRouter();
 
-  const handleSubmitUsername = async () => {
-    const user = await useSupabaseUser();
-    if (!user?.id) return;
+  const {
+    control,
+    handleSubmit,
+    setError,
+    formState: { errors },
+  } = useForm<UsernameForm>({
+    defaultValues: {
+      username: "",
+    },
+    shouldFocusError: true,
+  });
 
-    if (username.length < 5) {
-      Alert.alert("Le pseudo doit faire au moins 5 caractères");
-      return;
-    }
+  /**
+   * Possible to upgrade with parallel requests
+   * @param inputs
+   * @returns
+   */
+  const onSubmit = async ({ username }: UsernameForm) => {
+    console.log("submitting", username);
+    console.log("profile", user);
+    if (!user) return;
+
+    console.log("submitting", username);
     const { error } = await supabase
       .from("user_profile")
       .update({ username })
-      .eq("account_id", user?.id);
-    if (error) {
-      if (error.code === SupabaseErrorCode.CONSTRAINT_VIOLATION) {
-        Alert.alert("Attention, Ce pseudo est déjà pris");
-        return;
-      }
-      Alert.alert("Erreur, Une erreur est survenue");
+      .eq("account_id", user.id);
+
+    if (!error) {
+      router.push("/");
     }
-    router.replace("/(tabs)");
+    if (error?.code === SupabaseErrorCode.CONSTRAINT_VIOLATION) {
+      setError("username", {
+        type: "manual",
+        message: "Ce nom d'utilisateur est déjà pris",
+      });
+    }
   };
 
   return (
-    <Screen>
-      <View style={styles.titleContainer}>
-        <Text style={styles.title}>Choisi ton pseudo</Text>
-      </View>
-      <View style={styles.form}>
-        <TextInput
-          value={username}
-          onChangeText={setUsername}
-          placeholder="Pseudo"
-          style={{
-            borderColor: "gray",
-            width: "100%",
-            borderWidth: 1,
-            borderRadius: 10,
-            padding: 10,
-          }}
-        />
-        <Button onPress={handleSubmitUsername}>Valider</Button>
-      </View>
+    <Screen
+      style={{
+        flex: 1,
+        alignItems: "stretch",
+        gap: 26,
+        padding: 16,
+      }}
+    >
+      <Text
+        style={{
+          // textAlign: "center",
+          marginVertical: 16,
+          fontFamily: Font.Outfit.Medium,
+          fontSize: 16,
+        }}
+      >
+        Ici, tu peux choisir un nom d'utilisateur pour que tes amis puissent te
+        trouver plus facilement. Ce nom est unique sur DATSMYSONG !
+      </Text>
+      <ControlledInput
+        autofocus
+        control={control}
+        label="Nom d'utilisateur"
+        name="username"
+        placeholder="@MonNomDutilisateur"
+        rules={usernameRules}
+        errorMessage={errors.username && errors.username.message}
+        onSubmitEditing={handleSubmit(onSubmit)}
+        info="Tu n'as le droit qu'à des lettres, des chiffres..."
+      />
+      <Button block onPress={handleSubmit(onSubmit)}>
+        Ajouter ce nom
+      </Button>
     </Screen>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    // justifyContent: "center",
-    alignItems: "center",
-    gap: 20,
-    width: "100%",
-  },
-  form: {
-    padding: 20,
-    alignItems: "center",
-    gap: 30,
-  },
-  buttonContainer: {
-    alignItems: "center",
-  },
-  title: {
-    fontSize: 30,
-    fontWeight: "bold",
-    textAlign: "center",
-  },
-  titleContainer: {
-    marginTop: 100,
-    marginBottom: 100,
-  },
-  containerWithDivider: {
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
