812f8d5819329c6b8c02cf3820d970fd2a7ba5f0
GaspardBBY <96123850+GaspardBBY@users.noreply.github.com>
2024-04-09 11:00:36 +0200

    docs: removed old wrong doc

--- backend/src/socketio/RoomIO.ts
+++ backend/src/socketio/RoomIO.ts
@@ -29,8 +29,8 @@ export default function onRoomWSConnection(socket: TypedSocket) {
   }
 
   const activeRoomId = rawUrlMatchGroups.slice(1)[0];
-  // TODO: Actually check if the connecting socket is the host
 
+  //! Host can tell the server that he is the host (not secure)
   const isHostSocket = socket.handshake.auth;
 
   async function registerHandlers() {
