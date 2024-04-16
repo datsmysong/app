5eea2a5b5d1922cb57318299ffe07202e42ac474
MAXOUXAX <24844231+MAXOUXAX@users.noreply.github.com>
2024-04-17 00:03:51 +0200

    style: removed background-color on every view

--- expo/components/Themed.tsx
+++ expo/components/Themed.tsx
@@ -44,11 +44,7 @@ export function Text(props: TextProps) {
 }
 
 export function View(props: ViewProps) {
-  const { style, lightColor, darkColor, ...otherProps } = props;
-  const backgroundColor = useThemeColor(
-    { light: lightColor, dark: darkColor },
-    "background"
-  );
+  const { style, ...otherProps } = props;
 
-  return <DefaultView style={[{ backgroundColor }, style]} {...otherProps} />;
+  return <DefaultView style={style} {...otherProps} />;
 }
