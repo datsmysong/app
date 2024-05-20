e5092362fdf2ab2477134c33bf43cf3fce62f48a
MAXOUXAX <24844231+MAXOUXAX@users.noreply.github.com>
2024-05-20 20:30:01 +0200

    feat: updated missing page

--- expo/app/[...missing].tsx
+++ expo/app/[...missing].tsx
@@ -1,15 +1,44 @@
-import { Link, Stack } from "expo-router";
-import { Text, View } from "react-native";
+import { Stack, useRouter } from "expo-router";
+import ArrowSquareOut from "phosphor-react-native/src/icons/ArrowSquareOut";
+import LinkBreak from "phosphor-react-native/src/icons/LinkBreak";
+import { View } from "react-native";
+
+import Button from "../components/ui/Button";
+import { H1 } from "../components/ui/typography/Titles";
 
 export default function NotFoundScreen() {
+  const expoRouter = useRouter();
+
+  const goBack = () => {
+    if (expoRouter.canGoBack()) {
+      expoRouter.back();
+    } else {
+      expoRouter.replace("/");
+    }
+  };
+
   return (
     <>
       <Stack.Screen options={{ title: "Oops!" }} />
-      <View>
-        <Text>This screen doesn't exist.</Text>
-        <Link href="/">
-          <Text>Go to home screen!</Text>
-        </Link>
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+          padding: 16,
+          gap: 64,
+        }}
+      >
+        <View
+          style={{ alignItems: "center", gap: 16, justifyContent: "center" }}
+        >
+          <LinkBreak size={64} />
+          <H1>Cette page n'existe pas</H1>
+        </View>
+        <Button onPress={goBack} block appendIcon={<ArrowSquareOut />}>
+          {expoRouter.canGoBack() && "Retourner à la page précédente"}
+          {!expoRouter.canGoBack() && "Retourner à la page d'accueil"}
+        </Button>
       </View>
     </>
   );
