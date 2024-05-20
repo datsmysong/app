51f5bd357bc674f10c1fc0f44ac29e7256c12686
MAXOUXAX <24844231+MAXOUXAX@users.noreply.github.com>
2024-05-20 20:09:14 +0200

    chore: removed JSDoc

--- expo/components/ui/typography/Paragraphs.tsx
+++ expo/components/ui/typography/Paragraphs.tsx
@@ -7,33 +7,21 @@ import { Text } from "../../Themed";
 
 /**
  * Subtitle component (Outfit Regular, 16px)
- *
- * @component
- * @param {TypographyComponentProps} props - The component props.
- * @param {React.ReactNode} props.children - The content of the subtitle.
- * @param {import("react-native").StyleProp<import("react-native").TextStyle>} props.style - The additional style to apply to the subtitle.
- * @returns {React.ReactElement} The rendered Subtitle component.
  */
 export const Subtitle: React.FC<TypographyComponentProps> = ({
   children,
   style,
-}) => {
+}: TypographyComponentProps): React.ReactElement => {
   return <Text style={[styles.subtitle, style]}>{children}</Text>;
 };
 
 /**
  * A medium subtitle component. (Outfit Regular, 18px)
- *
- * @component
- * @param {TypographyComponentProps} props - The component props.
- * @param {React.ReactNode} props.children - The content of the subtitle.
- * @param {import("react-native").StyleProp<TextStyle>} props.style - The additional style to apply to the subtitle.
- * @returns {React.ReactElement} The rendered `MediumSubtitle` component.
- * */
+ */
 export const MediumSubtitle: React.FC<TypographyComponentProps> = ({
   children,
   style,
-}) => {
+}: TypographyComponentProps): React.ReactElement => {
   return (
     <Text style={[styles.subtitle, styles.mediumSubtitle, style]}>
       {children}
@@ -43,17 +31,11 @@ export const MediumSubtitle: React.FC<TypographyComponentProps> = ({
 
 /**
  * A big subtitle component. (Outfit Regular, 20px)
- *
- * @component
- * @param {TypographyComponentProps} props - The component props.
- * @param {React.ReactNode} props.children - The content of the subtitle.
- * @param {StyleProp<TextStyle>} props.style - The additional style to apply to the subtitle.
- * @returns {React.ReactElement} The rendered `BigSubtitle` component.
  */
 export const BigSubtitle: React.FC<TypographyComponentProps> = ({
   children,
   style,
-}) => {
+}: TypographyComponentProps): React.ReactElement => {
   return (
     <Text style={[styles.subtitle, styles.bigSubtitle, style]}>{children}</Text>
   );
--- expo/components/ui/typography/Titles.tsx
+++ expo/components/ui/typography/Titles.tsx
@@ -10,47 +10,41 @@ export type TypographyComponentProps = {
 
 /**
  * H1 component for displaying level 1 headings. (Unbounded Bold, 32px)
- *
- * @component
- * @param {TypographyComponentProps} props - The component props.
- * @param {React.ReactNode} props.children - The content of the heading.
- * @param {StyleProp<TextStyle>} props.style - The additional styles to apply to the heading.
- * @returns {React.ReactElement} The rendered level 1 heading component.
  */
-export const H1: React.FC<TypographyComponentProps> = ({ children, style }) => {
+export const H1: React.FC<TypographyComponentProps> = ({
+  children,
+  style,
+}: TypographyComponentProps): React.ReactElement => {
   return <Text style={[styles.h1, style]}>{children}</Text>;
 };
 
 /**
  * H2 component for displaying level 2 headings. (Outfit Bold, 24px)
- *
- * @param children - The content to be displayed within the H2 component.
- * @param style - Additional styles to be applied to the H2 component.
- * @returns The rendered H2 component.
  */
-export const H2: React.FC<TypographyComponentProps> = ({ children, style }) => {
+export const H2: React.FC<TypographyComponentProps> = ({
+  children,
+  style,
+}: TypographyComponentProps): React.ReactElement => {
   return <Text style={[styles.h2, style]}>{children}</Text>;
 };
 
 /**
  * H3 component for displaying level 3 headings. (Outfit Bold, 20px)
- *
- * @param children - The content to be displayed within the H3 component.
- * @param style - Additional styles to be applied to the H3 component.
- * @returns The rendered H3 component.
  */
-export const H3: React.FC<TypographyComponentProps> = ({ children, style }) => {
+export const H3: React.FC<TypographyComponentProps> = ({
+  children,
+  style,
+}: TypographyComponentProps): React.ReactElement => {
   return <Text style={[styles.h3, style]}>{children}</Text>;
 };
 
 /**
  * H4 component for displaying level 4 headings. (Outfit Bold, 18px)
- *
- * @param children - The content to be displayed within the H4 component.
- * @param style - Additional styles to be applied to the H4 component.
- * @returns The rendered H4 component.
  */
-export const H4: React.FC<TypographyComponentProps> = ({ children, style }) => {
+export const H4: React.FC<TypographyComponentProps> = ({
+  children,
+  style,
+}: TypographyComponentProps): React.ReactElement => {
   return <Text style={[styles.h4, style]}>{children}</Text>;
 };
 
