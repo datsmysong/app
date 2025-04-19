a66f8317dbdca2e9d808def000caf8511a726d6e
MAXOUXAX <24844231+MAXOUXAX@users.noreply.github.com>
2025-04-19 18:57:01 +0200

    feat(expo): improve 404 page

--- expo/app/[...missing].tsx
+++ expo/app/[...missing].tsx
@@ -1,16 +1,48 @@
-import { Link, Stack } from "expo-router";
-import { Text, View } from "react-native";
+import { Stack, useRouter } from "expo-router";
+import ArrowSquareOut from "phosphor-react-native/src/icons/ArrowSquareOut";
+import LinkBreak from "phosphor-react-native/src/icons/LinkBreak";
+import { ScrollView, View } from "react-native";
+
+import Button from "../components/ui/Button";
+import { Subtitle } from "../components/ui/typography/Paragraphs";
+import { H1 } from "../components/ui/typography/Titles";
 
 export default function NotFoundScreen() {
+  const expoRouter = useRouter();
+
+  const goBack = () => {
+    if (expoRouter.canGoBack()) {
+      expoRouter.back();
+    } else {
+      expoRouter.replace("/");
+    }
+  };
+
   return (
     <>
       <Stack.Screen options={{ title: "Oops!" }} />
-      <View>
-        <Text>This screen doesn't exist.</Text>
-        <Link href="/">
-          <Text>Go to home screen!</Text>
-        </Link>
-      </View>
+      <ScrollView
+        contentContainerStyle={{
+          flex: 1,
+          justifyContent: "center",
+          padding: 32,
+          gap: 64,
+          maxWidth: 512,
+        }}
+      >
+        <View style={{ gap: 16, justifyContent: "center" }}>
+          <LinkBreak size={64} />
+          <H1>Cette page n'existe pas</H1>
+          <Subtitle>
+            La page que vous essayez d'atteindre n'existe pas ou a été
+            supprimée.
+          </Subtitle>
+        </View>
+        <Button onPress={goBack} block appendIcon={<ArrowSquareOut />}>
+          {expoRouter.canGoBack() && "Retourner à la page précédente"}
+          {!expoRouter.canGoBack() && "Retourner à la page d'accueil"}
+        </Button>
+      </ScrollView>
     </>
   );
 }
